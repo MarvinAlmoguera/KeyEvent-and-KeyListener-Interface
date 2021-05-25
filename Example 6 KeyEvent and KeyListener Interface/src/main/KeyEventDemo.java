8641f2278dc0ead288f011db44733f053283e2c1
Mrvnqty <84437917+Mrvnqty@users.noreply.github.com>
2021-05-25 16:57:44 +0800

    Add files via upload

---
+++ Example 6 KeyEvent and KeyListener Interface/src/main/KeyEventDemo.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Lenovo
+ *
+ */
+import java.awt.*;
+import java.awt.event.*;
+
+public class KeyEventDemo extends Frame {
+
+	private TextField tfInput;
+	private TextArea taDisplay;
+		
+	public KeyEventDemo() {
+		setLayout(new FlowLayout());
+		
+		add(new Label("Enter Text: "));
+		tfInput = new TextField(10);
+		add(tfInput);
+		taDisplay = new TextArea(5, 40);
+		add(taDisplay);
+		
+		setTitle("KeyEvent Demo");
+		setSize(400, 200);
+		setVisible(true);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new KeyEventDemo();
+	}
+
+}
